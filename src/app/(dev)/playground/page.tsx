0680729529ef6ee33fb8f6f671832eb10fe0cdf0
DSC NIT Rourkela <dsc.nitr@gmail.com>
2025-08-20 22:40:07 +0530

    fix: delete gazebo and some refactoring

--- src/app/(dev)/playground/page.tsx
+++ src/app/(dev)/playground/page.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { HackNitr } from '@/components/hero/hackNitr';
-
-import { HeroMobile } from '@/components/hero/heroMobile';
-
-import Clouds from '@/components/hero/clouds';
+import Hero from '@/components/hero/hero';
 
 export default function Page() {
   return (
     <>
-      <div className="">
-        <div className=" min-h-[90vh] hidden lg:flex justify-center items-center">
-          <HackNitr />
-        </div>
-        <div className=" pb-[15svh] py-14 lg:hidden ">
-          <HeroMobile />
-        </div>
-
-        <Clouds />
-      </div>
+      <Hero />
     </>
   );
 }
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,19 @@
+import { HackNitr } from '@/components/hero/hackNitr';
+import { HeroMobile } from '@/components/hero/heroMobile';
+import Clouds from '@/components/hero/clouds';
+
+
+export default function Hero(){
+    return (
+        <>
+        <div className=" min-h-[90vh] hidden lg:flex justify-center items-center">
+          <HackNitr />
+        </div>
+        <div className=" pb-[15svh] py-14 lg:hidden ">
+          <HeroMobile />
+        </div>
+        <Clouds />
+        </>
+
+    )
+}
