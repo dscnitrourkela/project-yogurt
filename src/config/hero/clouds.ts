22648b445ed2e0f27f16b41b9befeaa9a7fc9e3c
ayush <ayushatschool5@gmail.com>
2025-08-27 02:07:01 +0530

    chore: update temp hero

--- src/config/hero/clouds.ts
+++ src/config/hero/clouds.ts
@@ -6,3 +6,9 @@ export const HERO_CLOUDS_CONFIG = {
       'https://res.cloudinary.com/dscnitrourkela/image/upload/v1754332162/hacknitr/t6hwqqvgad8dypakamwt.png',
   },
 } as const;
+
+export const TEMP_HERO_CONFIG = {
+  main: 'https://res.cloudinary.com/dscnitrourkela/image/upload/v1756239330/hacknitr/rscbwiivneoytsgr2s79.png',
+  dates:
+    'https://res.cloudinary.com/dscnitrourkela/image/upload/v1756239329/hacknitr/uikw0qkx9krornmqj6yp.png',
+};
