e38cc5eda4e4ecfdeb8b7e577600848a74d01777
aishwary jhunjhunwala <aishwaryjjw@gmail.com>
2025-08-27 01:27:43 +0530

    About us updated

--- src/app/(dev)/playground/page.tsx
+++ src/app/(dev)/playground/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from '@/components/about/about';
 import Hero from '@/components/hero/hero';
+import Footer from '@/components/marginals/footer';
 
 export default function Page() {
   return (
     <>
       <Hero />
       <About />
+      <Footer />
     </>
   );
 }
