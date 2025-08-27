c91230d21563edae84829faf34b987728d8651d0
DevSw <64429051+devsw-prayas@users.noreply.github.com>
2025-08-27 21:18:27 +0530

    feat: Updated FAQs content with latest provided.

--- src/config/faq/index.ts
+++ src/config/faq/index.ts
@@ -9,15 +9,16 @@ export interface Question {
 export const questions: Question[] = [
   {
     question: 'What are the dates for HackNITR 7.0?',
-    answer: 'Coming Soon!',
-    answerStyle: 'h-25',
-    questionStyle: 'mb-15 -bottom-15',
-    fontSizeScaling: 'text-[clamp(0.8rem,2vw,1rem)]',
+    answer:
+      'HackNITR 7.0 will be held on 31st Oct 1st Nov and 2nd Nov 2025. We will be waiting for you with a warm welcome.\n',
+    answerStyle: 'h-35',
+    questionStyle: 'mb-25 -bottom-25',
+    fontSizeScaling: 'text-[clamp(.85rem,2vw,1rem)]',
   },
   {
     question: 'What could be the size of the team?',
     answer:
-      "We're only looking for groups of 2 to 4 heroes this time. No solo adventurers allowed. Gather your buddies and get ready for an epic adventure together!",
+      "We're only looking for groups of 2 to 4 heroes this time. No solo adventurers allowed. Gather your buddies and get ready for an epic adventure together!\n",
     answerStyle: 'h-40',
     questionStyle: 'mb-30 -bottom-30',
     fontSizeScaling: 'text-[clamp(0.8rem,3vw,1rem)]',
@@ -41,8 +42,8 @@ export const questions: Question[] = [
   {
     question: 'Who can participate?',
     answer:
-      'Any student with a zeal to innovate and have some fun with creativity can apply. But the status of your ' +
-      'application can be changed anytime by the organizing team. The final decision is vested with the organizing team and the candidature can be changed based on the undertaking.',
+      'Any student with a zeal to innovate and have some fun with creativity can apply. But the status of your application can be changed anytime by the organizing team. ' +
+      'The final decision is vested with the organizing team and the candidature can be changed based on the undertaking.',
     answerStyle: 'h-60',
     questionStyle: 'mb-45 -bottom-45',
     fontSizeScaling: 'text-[clamp(0.75rem,3vw,1rem)]',
@@ -61,7 +62,7 @@ export const questions: Question[] = [
     answer:
       'Although we will provide you with a lot of things, you should bring your laptop, chargers, extension cords, and any other hardware you might need. Furthermore it is mandatory' +
       ' to bring a physical copy of your undertaking form signed by your college authority and the hacker ID assigned to you within 24 hrs before the event along with a valid govt. ID. ' +
-      'For further information please look at Things to carry for HackNITR 6.0 notion link. Students failing to submit the MoU within the stipulated time will be disqualified from the event.',
+      'For further information please look at Things to carry for HackNITR 7.0 notion link. Students failing to submit the MoU within the stipulated time will be disqualified from the event.',
     answerStyle: 'h-85',
     questionStyle: 'mb-73 -bottom-73',
     fontSizeScaling: 'text-[clamp(0.75rem,3vw,1rem)]',
