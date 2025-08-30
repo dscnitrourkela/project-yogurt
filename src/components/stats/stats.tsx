eb3f180f342045d069e33c77744e5b83dd21409b
HIMANSHU6001 <hk9797592893@gmail.com>
2025-08-30 13:56:16 +0530

    fix: stats section height fix

--- src/components/stats/stats.tsx
+++ src/components/stats/stats.tsx
@@ -18,7 +18,9 @@ const StatItem = React.memo<StatItemProps>(({ stat, index, isMobile }) => (
     <Typography.H3 className="font-wc-rough-trad text-center">
       {stat.number}
     </Typography.H3>
-    <Typography.P className="text-center text-black">{stat.label}</Typography.P>
+    <Typography.P className="text-center font-semibold text-black">
+      {stat.label}
+    </Typography.P>
     <Image
       src={index % 2 === 0 ? IMAGES.barLong : IMAGES.barShort}
       alt="bar"
@@ -46,29 +48,33 @@ const MobileStatItem = React.memo<MobileStatItemProps>(
     return (
       <div className="flex flex-col mt-auto">
         {index === 1 && (
-          <div className="mb-20">
+          <div className="mb-10">
             <Typography.H3 className="font-wc-rough-trad text-center">
               {statsData[0]?.number}
             </Typography.H3>
-            <Typography.P className="text-center text-black">
+            <Typography.P className="text-center font-semibold text-black">
               {statsData[0]?.label}
             </Typography.P>
           </div>
         )}
         {index === 3 && (
-          <div className="mb-20">
+          <div className="mb-10">
             <Typography.H3 className="font-wc-rough-trad text-center">
               {statsData[4]?.number}
             </Typography.H3>
-            <Typography.P className="text-center text-black">
+            <Typography.P className="text-center font-semibold text-black">
               {statsData[4]?.label}
             </Typography.P>
           </div>
         )}
-        <Typography.H3 className="font-wc-rough-trad text-center">
+        <Typography.H3
+          className={`font-wc-rough-trad text-center ${index === 2 ? 'scale-150' : 'scale-100'}`}
+        >
           {stat.number}
         </Typography.H3>
-        <Typography.P className="text-center text-black">
+        <Typography.P
+          className={`text-center font-semibold text-black ${index === 2 ? 'scale-150' : 'scale-100'}`}
+        >
           {stat.label}
         </Typography.P>
         <Image
@@ -118,7 +124,10 @@ const Stats: React.FC = () => {
   }, [isMobile]);
 
   return (
-    <div className="relative h-screen" id="stats">
+    <div
+      className={`relative ${isMobile ? 'h-[90vh]' : 'h-screen'}`}
+      id="stats"
+    >
       <Typography.H1 className="text-center font-normal font-wc-rough-trad text-blue-800 text-[clamp(3.5rem,5vw,6rem)]">
         STATS
       </Typography.H1>
