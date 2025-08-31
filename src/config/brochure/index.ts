8b7177b5071331c2f340173328ecaaf73d81e7ae
DSC NIT Rourkela <dsc.nitr@gmail.com>
2025-09-01 01:24:05 +0530

    Update index.ts

--- src/config/brochure/index.ts
+++ src/config/brochure/index.ts
@@ -11,14 +11,14 @@ export const brochures: Brochure[] = [
     id: 2,
     title: 'Sponsorship Brochure',
     pdfUrl:
-      'https://drive.google.com/file/d/1UId3i8KPrWFJC-oQAYoguCMD8l3ynI2L/view',
+      'https://drive.google.com/file/d/1-3HYx8s8zRwyLSNof3UXHn-xJRBKLaT2/view',
     description: 'Partnership and sponsorship opportunities',
     comingSoon: false,
   },
   {
     id: 1,
     title: 'Event Brochure',
-    pdfUrl: null,
+    pdfUrl: 'https://drive.google.com/file/d/1xUPlUUJJGuch4gge_73uZGEAJRjaAoPW/view',
     description: 'Complete details about HackNITR 7.0',
     comingSoon: true,
   },
