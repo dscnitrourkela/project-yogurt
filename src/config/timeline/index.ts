c1e10cd43057dfcdbba8d93586fd35098e1030d4
Ayush <135319056+ayussh-2@users.noreply.github.com>
2025-08-31 20:22:56 +0530

    Update event duration message for registrations

--- src/config/timeline/index.ts
+++ src/config/timeline/index.ts
@@ -3,7 +3,8 @@ export const timelineData = {
     {
       eventNumber: 1,
       title: 'Registrations Open',
-      duration: 'Duration - 2nd Sept to 15 Oct',
+      // duration: 'Duration - 2nd Sept to 15 Oct',
+      duration: 'Opening Soon...',
       description: `All Aboard! Save your spot at India's largest student-run hackathon.`,
       className:
         'absolute lg:top-[125%]  lg:left-[7%] xlg:left-[6%] xl:left-[12%]',
