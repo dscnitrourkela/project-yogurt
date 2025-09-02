44774774bcb32fee787daff33a0bb6537a43867e
Krish Agarwal <agarwalkrish526@gmail.com>
2025-09-02 22:53:08 +0530

    Update: Clue 1 code

--- src/components/ASCII/ASCIIlog.tsx
+++ src/components/ASCII/ASCIIlog.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect } from 'react';
-import Clue from '../clue-1/clue1';
 
 export default function AsciiLogger() {
   useEffect(() => {
@@ -20,10 +19,21 @@ export default function AsciiLogger() {
    ##+++++++++++++++++++#+   
       #+#############+#      
    ##+++++#########++++++
-       HACKNITR 7.0`,
+
+The console spoke, but its voice grows faint.
+If you wish to find the next clue’s paint,
+Follow the path where whispers travel,
+Requests and answers slowly unravel`,
       'font-family: monospace; white-space: pre; line-height: 1.2;'
     );
+    console.error(`Error 404: Help Xori Reach Home`);
+    console.info(`You get two clues here`);
+    console.warn(`clue-a 
+    Two letters lead the way.One begins Javascript,the other starts Tools.Put them side by side to begin your journey`);
+    console.warn(`clue-b
+    Three Stars Shine, but only one is bright. The number of the brightest is the one you see.
+    `);
   }, []);
 
-  return <Clue />;
+  return null;
 }
